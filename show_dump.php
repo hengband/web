8ecfa11e6d22e40b9a675adf35e0e5706fdfb512
Habu <habu@users.sourceforge.jp>
2018-03-15 09:57:03 +0900

    [add]ダンプ表示用コード
    ファイルとして保存されているキャラクタダンプとスクリーンショットの表示

---
+++ show_dump.php
@@ -0,0 +1,10 @@
+<?php
+//ini_set('display_errors', 'On');
+
+ini_set('log_errors', 'On');
+ini_set('error_log', 'errors/'.pathinfo(__FILE__, PATHINFO_FILENAME).'.log');
+
+include "dump_file.inc";
+
+$dump_file = new DumpFile($_GET['score_id']);
+$dump_file->show('dumps', 'txt', 'text/plain; charset=UTF-8');
