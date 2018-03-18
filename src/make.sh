84b3764e537d74a26da222f5208af8678fe2edfd
Deskull <desull@users.sourceforge.jp>
2018-03-18 16:46:58 +0900

    著作権表記を整理。 / Rearrange license documents.

--- src/make.sh
+++ src/make.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-list="index web_update"
+list="index web_update jlicense"
+
+mkdir -p ../web
+cp -r image *.txt hengband.css ../web
 
 for v in $list; do
 	echo ${v}
 	asciidoctor --no-header-footer --out-file=tmp.txt \
 		--backend=html5 -a linkcss -a stylesheet=hengband.css \
 		${v}.adoc 
-	cat template.html | sed '/<!--main contents-->/r tmp.txt' > ${v}.html
+	cat template.html | sed '/<!--main contents-->/r tmp.txt' > ../web/${v}.html
 done
- 
+
+rm tmp.txt
+
