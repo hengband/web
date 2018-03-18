486107723cf1d2e2fc5b69b9c28a939b9face9e3
Deskull <desull@users.sourceforge.jp>
2018-03-18 14:58:47 +0900

    トップページの整理まで。 / To Rearrange top page.

--- src/make.sh
+++ src/make.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
-asciidoctor --no-header-footer  --out-file=tmp.txt --backend=html5 -a linkcss -a stylesheet=hengband.css web_update.adoc 
+#!/bin/bash
 
-cat template.html | sed '/<!--main contents-->/r tmp.txt' > web_update.html
+list="index web_update"
+
+for v in $list; do
+	echo ${v}
+	asciidoctor --no-header-footer --out-file=tmp.txt \
+		--backend=html5 -a linkcss -a stylesheet=hengband.css \
+		${v}.adoc 
+	cat template.html | sed '/<!--main contents-->/r tmp.txt' > ${v}.html
+done
+ 
