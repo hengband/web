8ac8c8427bb2056c683631a857272e1cad302264
Deskull <desull@users.sourceforge.jp>
2018-03-24 19:32:48 +0900

    スコアサーバとの連携のために、テンプレートを細分化 / Separate web templates for application to score server. 2018/03/19の更新 / Update 2018/03/19.

--- src/make.sh
+++ src/make.sh
@@ -10,7 +10,11 @@ for v in $list; do
 	asciidoctor --no-header-footer --out-file=tmp.txt \
 		--backend=html5 -a linkcss -a stylesheet=hengband.css \
 		${v}.adoc 
-	cat template.html | sed '/<!--main contents-->/r tmp.txt' > ../web/${v}.html
+	cat template.html | sed '/<!--main contents-->/r tmp.txt' \
+	 | sed '/<!--head-->/r head.html' \
+	 | sed '/<!--header-->/r header.html' \
+	 | sed '/<!--footer-->/r footer.html' \
+	 > ../web/${v}.html
 done
 
 rm tmp.txt
