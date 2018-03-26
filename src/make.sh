2203e66ae229a313a01df8d07767ccfaa0842308
Deskull <deskull@users.sourceforge.jp>
2018-03-27 02:12:36 +0900

    Ver1.0.8の履歴をChangeLogs.txtより反映。一部テンプレート整理中。 / History of Ver1.0.8 reflects from ChangeLogs.txt. Web templates is fixed partly.

--- src/make.sh
+++ src/make.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-list="index web_update jlicense link download lists history score"
-
-mkdir -p ../web
+mkdir -p ../web ../web/history
 cp -r image *.txt hengband.css ../web
+
+list="index web_update jlicense link download lists history score "
 
 for v in $list; do
 	echo ${v}
@@ -15,6 +15,20 @@ for v in $list; do
 	 | sed '/<!--header-->/r header.html' \
 	 | sed '/<!--footer-->/r footer.html' \
 	 > ../web/${v}.html
+done
+
+history_list="history1.0.8"
+
+for v in $history_list; do
+	echo ${v}
+	asciidoctor --no-header-footer --out-file=tmp.txt \
+		--backend=html5 -a linkcss -a stylesheet=../hengband.css \
+		history/${v}.adoc 
+	cat template.html | sed '/<!--main contents-->/r tmp.txt' \
+	 | sed '/<!--head-->/r head.html' \
+	 | sed '/<!--header-->/r header.html' \
+	 | sed '/<!--footer-->/r footer.html' \
+	 > ../web/history/${v}.html
 done
 
 rm tmp.txt
