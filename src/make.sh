6dc001ac3d4a773f5b7b8a8d4e05074a3ba7ab92
Deskull <deskull@users.sourceforge.jp>
2018-03-27 21:09:28 +0900

    スタイルシート修正。スコアページのルートにRSSとTwitterのリンク追加。 / Fix stylesheet. Add Twitter and RSS link to root of score page.

--- src/make.sh
+++ src/make.sh
@@ -3,7 +3,7 @@
 mkdir -p ../web ../web/history
 cp -r image *.txt hengband.css ../web
 
-list="index web_update jlicense link download lists history score "
+list="index web_update jlicense link download lists history"
 
 for v in $list; do
 	echo ${v}
@@ -12,6 +12,21 @@ for v in $list; do
 		${v}.adoc 
 	cat template.html | sed '/<!--main contents-->/r tmp.txt' \
 	 | sed '/<!--head-->/r head.html' \
+	 | sed '/<!--header-->/r header.html' \
+	 | sed '/<!--footer-->/r footer.html' \
+	 > ../web/${v}.html
+done
+
+score_list="score"
+
+for v in $score_list; do
+	echo ${v}
+	asciidoctor --no-header-footer --out-file=tmp.txt \
+		--backend=html5 -a linkcss -a stylesheet=hengband.css \
+		${v}.adoc
+	cat scorelinks.html >> tmp.txt
+	cat template.html | sed '/<!--main contents-->/r tmp.txt' \
+	 | sed '/<!--head-->/r head_score.html' \
 	 | sed '/<!--header-->/r header.html' \
 	 | sed '/<!--footer-->/r footer.html' \
 	 > ../web/${v}.html
